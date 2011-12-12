d84602d4428440dccc59406f80d5f221aaa2672a
Joseph Santaniello <joseph.santaniello@gmail.com>
2011-12-12 18:21:36 +0100

    Playing with small g

--- diffiehellman.py
+++ diffiehellman.py
@@ -4,15 +4,18 @@ p = long("fd7f53811d75122952df4a9c2eece4e7f611b7523cef4400c31e3f80b6512669455d40
 g = long("f7e1a085d69b3ddecbbcab5c36b857b97994afbbfa3aea82f9574c0b3d0782675159578ebad4594fe67107108180b449167123e84c281613b7cf09328cc8a6e13c167a8b547c8d28e0a3ae1e2bb3a675916ea37f0bfa213562f1fb627a01243bcca4f1bea8519089a883dfe15ae59f06928b665e807b552564014c3bfecf492a", 16)
 q = long("9760508f15230bccb292b982a2eb840bf0581cf5", 16)
 
+g = 2L
+
 # Alice
-x = random.randint(1,50)
+x = random.getrandbits(8)
 gx = pow(g, x)
 # Bob
-y = random.randint(1,50)
+y = random.getrandbits(8)
 gy = pow(g, y)
 
 Ka = pow(gy, x) % q
 Kb = pow(gx, y) % q
+print Kb
 print (Ka == Kb)
 print hashlib.md5(str(Kb)).hexdigest()
 print hashlib.md5(str(Ka)).hexdigest()
